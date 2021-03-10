b26113e20fa6bc7f955e979707801cd839fbb950
Brycen Martin <bmartin22@my.whitworth.edu>
2021-03-09 17:47:42 -0800

    added comments for code

--- source/Reverse.asm
+++ source/Reverse.asm
@@ -1,6 +1,6 @@
 .data
-    string1: .asciiz "Hello World"
-    string2: .space 256
+    string1: .asciiz "Hello World"  # string to reverse
+    string2: .space 256  # string to put reversed string into
 
 .text
 .globl main
@@ -9,18 +9,18 @@ main:
     addi $sp, $sp, -48   	# stack frame
     sw   $ra, 0($sp)    	# store $ra
 
-    la $s2, string2
-    la $s1, string1
+    la $s2, string2         # load string2
+    la $s1, string1         # load string1
 
-    GetChar:
-        lb $t0, ($s1)
-        beq $t0, $s0, exit
-        sb $t0, ($sp)
-        addi $s1, $s1, 1
-        addi $sp, $sp, 4
-        j GetChar
+    GetChar:                # method to break up words
+        lb $t0, ($s1)       # load current byte/char of word
+        beq $t0, $s0, exit  # check if char is = to zero if not exit
+        sb $t0, ($sp)       # store byte into stack
+        addi $s1, $s1, 1    # increment position to get next char
+        addi $sp, $sp, 4    # increment location in stack
+        j GetChar           # jump back to top of method
     exit:
 
-    lw $s2, string2
+    lw $s2, string2         # load string2
 
     jr $ra            	# return to OS
